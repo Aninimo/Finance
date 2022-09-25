702a09393363a463f2562a0eb7b7c683e3c9f62b
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-09-25 13:39:28 -0300

    Create styles.js

---
+++ src/components/TableItems/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const TableContainer = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  margin-top: 1.5rem;
+  background: var(--purple-500);
+  color: #fff;
+  padding: 1rem;
+  box-shadow: 8px 8px 8px 7px rgba(0, 0, 0, 0.2);
+  p{
+    margin-bottom: 1rem;
+  }
+`
