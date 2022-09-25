1324d2d5d7fb34fc522a0f98f39926f91962e7a7
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-09-25 13:36:51 -0300

    Create styles.js

---
+++ src/components/Form/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const FormContainer = styled.button`
+  padding: .5rem 3rem;
+  margin-bottom: 1rem;
+  background: var(--purple-500);
+  color: #fff;
+  border-radius: 8px;
+  border: none;
+  cursor: pointer;
+  transition: .5s;
+  :hover{
+    background: var(--purple-hover);
+  }
+`
